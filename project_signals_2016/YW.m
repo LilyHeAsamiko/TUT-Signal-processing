e671f0fd487468e3e7e328f255ff52e9c6f1db71
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2019-01-17 22:08:02 +0200

    Delete YW.m

--- project_signals_2016/YW.m
+++
@@ -1,9 +0,0 @@
-function [A,sigma] =YW(s , order)
-r = xcorr(s,order);
-r = r(order+1:end);
-T = toeplitz(r);
-tmp_T = T(1:end-1,1:end-1);
-A = -(tmp_T\T(2:end,1));
-A = [1; A];
-sigma = T(1,:)*A;
-end
